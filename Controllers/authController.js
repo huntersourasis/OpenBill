5d6e597da699af15fe796b21aa33ddc36d9ea680
huntersourasis <clashersourasis@gmail.com>
2026-04-20 06:17:32 +0530

    Update : Bcrypt - Bcryptjs

--- Controllers/authController.js
+++ Controllers/authController.js
@@ -1,85 +1,87 @@
-import bcrypt from 'bcrypt';
-import {User} from "../Modals/userModal.js";
-import getCurrentDate from '../Utils/currentDate.js';
-import { sendHttpResponse } from '../Utils/httpResponse.js';
-import loadSettings from '../Utils/loadSettings.js';
-import jwt from 'jsonwebtoken';
-const loginController = async (req , res)=>{
-    try 
-    {
-        const {email , password} = req.body;
-        const isEmailExist = await User.findOne({email});
-        if(!isEmailExist)
-        {
-            return sendHttpResponse(res , 401 , false , "Invalid Email");
-        }
-        const status = isEmailExist.status;
-        if(!status)
-        {
-            return sendHttpResponse(res , 401 , false , "This user is currently deactivated");
-        }
-        const settings = await loadSettings();
-        const expiresIn = settings.sessionTimeout;
-        const maxLoginAttempts = isEmailExist?.maxLoginAttempts ?? 0;
-        if(maxLoginAttempts >= settings.maxLoginAttempts)
-        {
-            isEmailExist.status = false;
-            await isEmailExist.save();
-            return sendHttpResponse(res , 401 , false , "Maximum login attempts reached");
-        }
-        const isMatch = await bcrypt.compare(password , isEmailExist.password);
-        if(isMatch)
-        {
-           const payload = {
-                id : isEmailExist._id,
-                fullname : isEmailExist.fullname,
-                email : isEmailExist.email,
-                role : isEmailExist.role
-           };
-
-           isEmailExist.lastlogin = getCurrentDate();
-           isEmailExist.maxLoginAttempts = 0;
-           await isEmailExist.save();
-
-
-           const secretKey = process.env.JWT_SECRET;
-
-           const jwt_token = jwt.sign(payload , secretKey , {expiresIn : `${expiresIn}m`});
-           
-           res.cookie('token' , jwt_token , {
-                httpOnly: true,  
-                sameSite: 'strict', 
-                maxAge: (3600000 * expiresIn)
-           })
-           return sendHttpResponse(res , 200 , true , "Login Successful" , payload);
-        } else
-        {
-           isEmailExist.maxLoginAttempts = isEmailExist.maxLoginAttempts + 1;
-           await isEmailExist.save(); 
-           return sendHttpResponse(res , 401 , false , "Incorrect Password");
-        }
+import bcrypt from "bcryptjs";
+import { User } from "../Modals/userModal.js";
+import getCurrentDate from "../Utils/currentDate.js";
+import { sendHttpResponse } from "../Utils/httpResponse.js";
+import loadSettings from "../Utils/loadSettings.js";
+import jwt from "jsonwebtoken";
+const loginController = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const isEmailExist = await User.findOne({ email });
+    if (!isEmailExist) {
+      return sendHttpResponse(res, 401, false, "Invalid Email");
     }
-    catch (error) 
-    {
-        return sendHttpResponse(res , 500 , false , "Internal Server Error");
+    const status = isEmailExist.status;
+    if (!status) {
+      return sendHttpResponse(
+        res,
+        401,
+        false,
+        "This user is currently deactivated",
+      );
     }
-}
+    const settings = await loadSettings();
+    const expiresIn = settings.sessionTimeout;
+    const maxLoginAttempts = isEmailExist?.maxLoginAttempts ?? 0;
+    if (maxLoginAttempts >= settings.maxLoginAttempts) {
+      isEmailExist.status = false;
+      await isEmailExist.save();
+      return sendHttpResponse(
+        res,
+        401,
+        false,
+        "Maximum login attempts reached",
+      );
+    }
+    const isMatch = await bcrypt.compare(password, isEmailExist.password);
+    if (isMatch) {
+      const payload = {
+        id: isEmailExist._id,
+        fullname: isEmailExist.fullname,
+        email: isEmailExist.email,
+        role: isEmailExist.role,
+      };
+
+      isEmailExist.lastlogin = getCurrentDate();
+      isEmailExist.maxLoginAttempts = 0;
+      await isEmailExist.save();
+
+      const secretKey = process.env.JWT_SECRET;
+
+      const jwt_token = jwt.sign(payload, secretKey, {
+        expiresIn: `${expiresIn}m`,
+      });
+
+      res.cookie("token", jwt_token, {
+        httpOnly: true,
+        sameSite: "strict",
+        maxAge: 3600000 * expiresIn,
+      });
+      return sendHttpResponse(res, 200, true, "Login Successful", payload);
+    } else {
+      isEmailExist.maxLoginAttempts = isEmailExist.maxLoginAttempts + 1;
+      await isEmailExist.save();
+      return sendHttpResponse(res, 401, false, "Incorrect Password");
+    }
+  } catch (error) {
+    return sendHttpResponse(res, 500, false, "Internal Server Error");
+  }
+};
 
 const logoutController = (req, res) => {
-    try {
-        const { isLoggedConfirm } = req.body;
+  try {
+    const { isLoggedConfirm } = req.body;
 
-        if (isLoggedConfirm) {
-            res.clearCookie('token', {
-                sameSite: 'strict'
-            });
-            return sendHttpResponse(res , 200 , true , "Logged out successfully");
-        }
-        return sendHttpResponse(res , 401 , false , "Logout confirmation required");
-
-    } catch (error) {
-        return sendHttpResponse(res , 500 , false , "Internal Server Error");
+    if (isLoggedConfirm) {
+      res.clearCookie("token", {
+        sameSite: "strict",
+      });
+      return sendHttpResponse(res, 200, true, "Logged out successfully");
     }
-}
+    return sendHttpResponse(res, 401, false, "Logout confirmation required");
+  } catch (error) {
+    return sendHttpResponse(res, 500, false, "Internal Server Error");
+  }
+};
 
-export {loginController , logoutController};
+export { loginController, logoutController };
--- Controllers/usersController.js
+++ Controllers/usersController.js
@@ -1,142 +1,144 @@
-import bcrypt from 'bcrypt';
+import bcrypt from "bcryptjs";
 import { User } from "../Modals/userModal.js";
-import { sendHttpResponse } from '../Utils/httpResponse.js';
+import { sendHttpResponse } from "../Utils/httpResponse.js";
 
 const createUserController = async (req, res) => {
-    try {
-        const { fullname, email, role, password, status } = req.body;
+  try {
+    const { fullname, email, role, password, status } = req.body;
 
-        if (!fullname || !email || !role || !password || status === undefined) {
-            return sendHttpResponse(res, 400, false, "JSON is not sufficient");
-        }
-
-        const existingUser = await User.findOne({ email });
-        if (existingUser) {
-            return sendHttpResponse(res, 409, false, "User already exists");
-        }
-
-        if(role === 'admin')
-        {
-            return sendHttpResponse(res , 401 , false , "Operation is not permitted.");
-        }
-
-        const hashedPass = await bcrypt.hash(password, 10);
-
-        const user = new User({
-            fullname,
-            email,
-            role,
-            password: hashedPass,
-            status
-        });
-
-        await user.save();
-        return sendHttpResponse(res, 201, true, `# Created User ${user.fullname}`);
-
-    } catch (error) {
-        console.error(error);
-        return sendHttpResponse(res, 500, false, "Internal server error");
+    if (!fullname || !email || !role || !password || status === undefined) {
+      return sendHttpResponse(res, 400, false, "JSON is not sufficient");
     }
+
+    const existingUser = await User.findOne({ email });
+    if (existingUser) {
+      return sendHttpResponse(res, 409, false, "User already exists");
+    }
+
+    if (role === "admin") {
+      return sendHttpResponse(res, 401, false, "Operation is not permitted.");
+    }
+
+    const hashedPass = await bcrypt.hash(password, 10);
+
+    const user = new User({
+      fullname,
+      email,
+      role,
+      password: hashedPass,
+      status,
+    });
+
+    await user.save();
+    return sendHttpResponse(res, 201, true, `# Created User ${user.fullname}`);
+  } catch (error) {
+    console.error(error);
+    return sendHttpResponse(res, 500, false, "Internal server error");
+  }
 };
 
 const readUserController = async (req, res) => {
-    try {
-        const users = await User.find().select("-password");
-        return sendHttpResponse(res, 200, true, "Users fetched successfully", users);
-    } catch (error) {
-        console.error(error);
-        return sendHttpResponse(res, 500, false, "Internal server error");
-    }
+  try {
+    const users = await User.find().select("-password");
+    return sendHttpResponse(
+      res,
+      200,
+      true,
+      "Users fetched successfully",
+      users,
+    );
+  } catch (error) {
+    console.error(error);
+    return sendHttpResponse(res, 500, false, "Internal server error");
+  }
 };
 
 const updateUserController = async (req, res) => {
-    try {
-        const {id, fullname, email, role, password, status } = req.body;
+  try {
+    const { id, fullname, email, role, password, status } = req.body;
 
-        if (role == 'admin')
-        {
-            return sendHttpResponse(res , 401 , false , "Operation is not permitted");
-        }
-
-        const isEmailExist = await User.findOne({email});
-
-
-        const user = await User.findById(id);
-        if (!user) {
-            return sendHttpResponse(res, 404, false, "User not found");
-        }
-
-        if(isEmailExist && user.email != email)
-        {
-            return sendHttpResponse(res , 401 , false , "Email is alreay acquried")
-        }
-
-        if (email) user.email = email; 
-        if (fullname) user.fullname = fullname;
-        if (role) user.role = role;
-        if (status !== undefined) user.status = status;
-
-        if (password) {
-            user.password = await bcrypt.hash(password, 10);
-        }
-
-        await user.save();
-        return sendHttpResponse(res, 200, true, `Updated User ${fullname}`);
-
-    } catch (error) {
-        console.error(error);
-        return sendHttpResponse(res, 500, false, "Internal server error");
+    if (role == "admin") {
+      return sendHttpResponse(res, 401, false, "Operation is not permitted");
     }
+
+    const isEmailExist = await User.findOne({ email });
+
+    const user = await User.findById(id);
+    if (!user) {
+      return sendHttpResponse(res, 404, false, "User not found");
+    }
+
+    if (isEmailExist && user.email != email) {
+      return sendHttpResponse(res, 401, false, "Email is alreay acquried");
+    }
+
+    if (email) user.email = email;
+    if (fullname) user.fullname = fullname;
+    if (role) user.role = role;
+    if (status !== undefined) user.status = status;
+
+    if (password) {
+      user.password = await bcrypt.hash(password, 10);
+    }
+
+    await user.save();
+    return sendHttpResponse(res, 200, true, `Updated User ${fullname}`);
+  } catch (error) {
+    console.error(error);
+    return sendHttpResponse(res, 500, false, "Internal server error");
+  }
 };
 
 const deleteUserController = async (req, res) => {
-    try {
-        const { id } = req.body;
+  try {
+    const { id } = req.body;
 
-        const deleted = await User.findByIdAndDelete(id);
-        if (!deleted) {
-            return sendHttpResponse(res, 404, false, "User not found");
-        }
-
-        return sendHttpResponse(res, 200, true, `# Deleted User ${deleted.fullname}`);
-
-    } catch (error) {
-        console.error(error);
-        return sendHttpResponse(res, 500, false, "Internal server error");
+    const deleted = await User.findByIdAndDelete(id);
+    if (!deleted) {
+      return sendHttpResponse(res, 404, false, "User not found");
     }
+
+    return sendHttpResponse(
+      res,
+      200,
+      true,
+      `# Deleted User ${deleted.fullname}`,
+    );
+  } catch (error) {
+    console.error(error);
+    return sendHttpResponse(res, 500, false, "Internal server error");
+  }
 };
 
-
 const userStatusController = async (req, res) => {
-    try {
-        const { id } = req.body;
+  try {
+    const { id } = req.body;
 
-        const user = await User.findById(id);
-        if (!user) {
-            return sendHttpResponse(res, 404, false, "User not found");
-        }
-
-        user.status = !user.status;
-        user.maxLoginAttempts = 0;
-        await user.save();
-
-        return sendHttpResponse(
-            res,
-            200,
-            true,
-            `# Activation Status (${user.fullname}) ${user.status ? "activated" : "deactivated"}`
-        );
-
-    } catch (error) {
-        console.error(error);
-        return sendHttpResponse(res, 500, false, "Internal server error");
+    const user = await User.findById(id);
+    if (!user) {
+      return sendHttpResponse(res, 404, false, "User not found");
     }
+
+    user.status = !user.status;
+    user.maxLoginAttempts = 0;
+    await user.save();
+
+    return sendHttpResponse(
+      res,
+      200,
+      true,
+      `# Activation Status (${user.fullname}) ${user.status ? "activated" : "deactivated"}`,
+    );
+  } catch (error) {
+    console.error(error);
+    return sendHttpResponse(res, 500, false, "Internal server error");
+  }
 };
 
 export {
-    createUserController,
-    readUserController,
-    updateUserController,
-    deleteUserController,
-    userStatusController
+  createUserController,
+  readUserController,
+  updateUserController,
+  deleteUserController,
+  userStatusController,
 };
